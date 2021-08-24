a4e728a0fef2da58ca3f1c20e1bef2ae1257181c
micheaol <micheaol80@gmail.com>
2021-08-24 18:19:11 +0000

    More styling for the calculator

--- script/main.js
+++ script/main.js
@@ -1,33 +1,29 @@
 //to get all the buttons on the page:
-const buttons = document.querySelectorAll('button');
-const currentDisplayScreen = document.querySelector('#current-screen');
+const buttons = document.querySelectorAll('.number');
+const outputScreen = document.querySelector('.output-screen');
+const dataOperatorBtns = document.querySelectorAll('#clear-button')
 
 
-//set a var to reset display screen
-let resetScreen = false;
 
-
-//add event listener to each button:
+//looping through number button:
 buttons.forEach((button) => {
-    button.addEventListener('click', appendNumber);
+    button.addEventListener('click', appendDigit);
+
 });
 
-//Click function to check which keypad is clicked
-function appendNumber(button) {
-    let btnInput = button.target.textContent;
-    if (currentDisplayScreen.textContent === '0' || resetScreen) {
-        currentDisplayScreen.textContent = "";
-    }
-    currentDisplayScreen.textContent += btnInput;
+
+
+//function to print number to the screen:
+function appendDigit(button) {
+
+    outputScreen.placeholder += button.target.textContent
 }
 
-//clear screen function:
+dataOperatorBtns.forEach((button) => {
+    button.addEventListener('click', clearScreen)
+});
 
-//Create an array of keypads' value:
-const keyPads = [
-    7, 8, 9, 'C', 'AC', 4, 5, 6, '÷', 'x', 1, 2, 3, '+', '-', '+/-', 0, '.', '=',
-]
-
-for (let counter = 0; counter < keyPads.length; counter++) {
-    // console.log(keyPads[counter])
+//function to clear the output screen:
+function clearScreen() {
+    outputScreen.placeholder = "";
 }
