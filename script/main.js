21d3a86d689257036aef196a8a91f8c52a3d91f1
micheaol <micheaol80@gmail.com>
2021-08-24 17:26:20 +0000

    change style.css file

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 //to get all the buttons on the page:
 const buttons = document.querySelectorAll('button');
-const currentDisplayScreen = document.querySelector('#currentDisplay-screen');
+const currentDisplayScreen = document.querySelector('#current-screen');
 
 
 //set a var to reset display screen
@@ -14,22 +14,15 @@ buttons.forEach((button) => {
 
 //Click function to check which keypad is clicked
 function appendNumber(button) {
-    //Get the value of the button clicked
     let btnInput = button.target.textContent;
-    //reset display screen if:
     if (currentDisplayScreen.textContent === '0' || resetScreen) {
         currentDisplayScreen.textContent = "";
     }
-    //set the value of button clicked to show in the display
     currentDisplayScreen.textContent += btnInput;
 }
 
 //clear screen function:
-function clearScreen() {
-    if (currentDisplayScreen.textContent === 'C') {
-        currentDisplayScreen.textContent = "";
-    }
-}
+
 //Create an array of keypads' value:
 const keyPads = [
     7, 8, 9, 'C', 'AC', 4, 5, 6, '÷', 'x', 1, 2, 3, '+', '-', '+/-', 0, '.', '=',
